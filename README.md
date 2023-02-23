23942551ebb4af1f42c56ecbe15e40aed2ad8da7
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-02-23 12:36:37 -0300

    tela de login

--- README.md
+++ README.md
@@ -1,16 +1,1 @@
-# fotos
-
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+UI inspirado em https://www.behance.net/gallery/145046493/Login-Sign-Up-UIUX-Page-Template-Design?tracking_source=search_projects%7Clogin
--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,11 +1,204 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:fotos/colors/app_colors.dart';
+import 'package:fotos/styles/app_styles.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+        backgroundColor: AppClors.backColor,
+        body: SizedBox(
+          height: height,
+          width: width,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Container(
+                  height: height,
+                  color: AppClors.mainBlueColor,
+                  child: Center(
+                    child: Text(
+                      'A aventura \ncomeça aqui',
+                      style: relewayStyle.copyWith(
+                          fontSize: 48.0,
+                          color: AppClors.whiteColor,
+                          fontWeight: FontWeight.w800),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: height * 0.1,
+              ),
+              Expanded(
+                child: Container(
+                  height: height,
+                  margin: EdgeInsets.symmetric(horizontal: height * 0.10),
+                  color: AppClors.backColor,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: height * 0.145,
+                      ),
+                      RichText(
+                          text: TextSpan(children: [
+                        TextSpan(
+                            text: 'Olá, ',
+                            style: relewayStyle.copyWith(
+                                fontSize: 25.0,
+                                color: AppClors.blueDarkColor,
+                                fontWeight: FontWeight.normal)),
+                        TextSpan(
+                            text: 'bem vindo de volta',
+                            style: relewayStyle.copyWith(
+                                fontWeight: FontWeight.w900,
+                                color: AppClors.blueDarkColor,
+                                fontSize: 25.0))
+                      ])),
+                      SizedBox(
+                        height: height * 0.03,
+                      ),
+                      Text(
+                        'Hey, insira seus dados \npara entrar na sua conta',
+                        style: relewayStyle.copyWith(
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.w400,
+                            color: AppClors.textColor),
+                      ),
+                      SizedBox(
+                        height: height * 0.064,
+                      ),
+                      Text(
+                        'Email',
+                        style: relewayStyle.copyWith(
+                            fontSize: 12.0,
+                            color: AppClors.blueDarkColor,
+                            fontWeight: FontWeight.w700),
+                      ),
+                      SizedBox(
+                        height: height * 0.02,
+                      ),
+                      Container(
+                        height: 50.0,
+                        width: width,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16.0),
+                            color: AppClors.whiteColor),
+                        child: TextFormField(
+                          style: relewayStyle.copyWith(
+                              fontWeight: FontWeight.w400,
+                              color: AppClors.blueDarkColor,
+                              fontSize: 12.0),
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              contentPadding: const EdgeInsets.only(top: 1.0),
+                              hintText: 'Entre com o e-mail',
+                              hintStyle: relewayStyle.copyWith(
+                                  fontWeight: FontWeight.w400,
+                                  color:
+                                      AppClors.blueDarkColor.withOpacity(0.5),
+                                  fontSize: 12.0)),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        'Senha',
+                        style: relewayStyle.copyWith(
+                            fontSize: 12.0,
+                            color: AppClors.blueDarkColor,
+                            fontWeight: FontWeight.w700),
+                      ),
+                      SizedBox(
+                        height: height * 0.02,
+                      ),
+                      Container(
+                        height: 50.0,
+                        width: width,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16.0),
+                            color: AppClors.whiteColor),
+                        child: TextFormField(
+                          style: relewayStyle.copyWith(
+                              fontWeight: FontWeight.w400,
+                              color: AppClors.blueDarkColor,
+                              fontSize: 12.0),
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              contentPadding: const EdgeInsets.only(top: 1.0),
+                              hintText: 'Entre com a senha',
+                              hintStyle: relewayStyle.copyWith(
+                                  fontWeight: FontWeight.w400,
+                                  color:
+                                      AppClors.blueDarkColor.withOpacity(0.5),
+                                  fontSize: 12.0)),
+                        ),
+                      ),
+                      SizedBox(
+                        height: height * 0.02,
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Ainda não tem uma conta? Cadastre-se',
+                              style: relewayStyle.copyWith(
+                                  fontSize: 12.0,
+                                  color: AppClors.mainBlueColor,
+                                  fontWeight: FontWeight.w300),
+                            )),
+                      ),
+                      SizedBox(
+                        height: height * 0.05,
+                      ),
+                      Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () {},
+                          borderRadius: BorderRadius.circular(16.0),
+                          child: Ink(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 70.0, vertical: 18.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16.0),
+                              color: AppClors.mainBlueColor,
+                            ),
+                            child: Text(
+                              'Sign In',
+                              style: relewayStyle.copyWith(
+                                fontWeight: FontWeight.w700,
+                                color: AppClors.whiteColor,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
